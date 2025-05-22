c2819d5ca029bf7215a0c3eb21708a292aa56f6a
Vs84303 <117506479+Vs84303@users.noreply.github.com>
2025-05-22 15:20:37 +0000

    Conversionofdatatypes

---
+++ Basics/Basics01/Conversionofdatatypes.js
@@ -0,0 +1,22 @@
+// let number1="1.6";
+//  let conversion= Number(number1);
+//  console.log(typeof (conversion));
+//  console.log(conversion);
+
+//  let number2="2vaibhav";
+//  console.log(typeof number2);
+//  let conversion2= Number(number2);
+//  console.log(typeof number2);
+//  console.log(conversion2);
+
+//  let num3=null;
+//   let conversion3= Number(num3);
+//  console.log(typeof num3);
+//  console.log(conversion3);
+
+
+ let num4=16;
+  let conversion4= Null(num4);
+ console.log(typeof num4);
+ console.log(conversion4);
+ console.log(typeof conversion4);

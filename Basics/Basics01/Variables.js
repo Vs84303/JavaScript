00278f90d52fb72cce0cd1e960865cf9391a129d
Vs84303 <117506479+Vs84303@users.noreply.github.com>
2025-05-22 14:18:50 +0000

    Variablescommit

---
+++ Basics/Basics01/Variables.js
@@ -0,0 +1,11 @@
+const accountId= 12;
+var accountName= "Vaibhav";
+let accountTye= "savings";
+ accountCity= "bareilly";
+ let accountGender;
+
+ console.log(accountId);
+//  accountId=45;
+ console.log(accountId);
+
+ console.table([accountId, accountName, accountTye, accountCity, accountGender]);

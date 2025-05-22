1eb70ebf50718b8703e66b68f5570ead13598961
Vs84303 <117506479+Vs84303@users.noreply.github.com>
2025-05-22 14:38:59 +0000

    Datatypes

---
+++ Basics/Basics01/Datatypes.js
@@ -0,0 +1,11 @@
+let number= 45;
+let name= "vaibhav";
+let alert= true;
+let gender= null;
+let hero=undefined;
+let vv=Symbol();
+
+console.log( typeof "vaibhav")
+console.log(typeof alert);
+console.log(typeof hero);
+console.log(typeof vv);

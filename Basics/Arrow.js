82ad8a74b0ab36074a3ddaaa1f5607ba9879aead
Vs84303 <117506479+Vs84303@users.noreply.github.com>
2025-05-23 15:43:05 +0000

    Arrow

---
+++ Basics/Arrow.js
@@ -0,0 +1,12 @@
+const user = {
+     username : "vaibhav",
+     price : 199,
+
+     welcomeMessage : function() {
+        console.log(`${this.username} , welcome to website`)
+
+     }
+  
+}
+
+console.log(user.welcomeMessage);
---
+++ Basics/Basics01/Function.js
@@ -0,0 +1,56 @@
+// function addTwoNumber (num1, num2){
+//     console.log(num1+num2)
+// }
+// addTwoNumber(3,4)
+
+
+// function addTwoNumber (num1, num2){
+//     let result= num1+num2
+//     return num1+num2;
+// }
+// addTwoNumber(3,4)
+
+// const result= addTwoNumber(3,4)
+// console.log(result)
+
+
+// function logInUserMessage(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(logInUserMessage("vaibhav"))
+
+//  function calculateCartPrice(...num1){ 
+// return num1;
+//  }
+
+//  console.log(calculateCartPrice(200,300,4,400,522));
+
+
+//  function calculateCartPrice(val1, val2,...num1){ 
+//  return num1;
+//  }
+
+//  console.log(calculateCartPrice(200,300,4,400,522));
+
+
+// const user = {
+//     userName:"vaibhav",
+//     userCity:"Bareilly"
+
+// }
+
+// function userDetails(anyobject){ 
+//     console.log(`The name of the user is ${anyobject.userName} and the city is ${anyobject.userCity}`)
+
+//  }
+
+//  userDetails(user)
+
+// const myNewArray = [1,2,3,4,5,6]
+
+// function arrayFunction(Arrayparameter){
+//     return Arrayparameter[1]
+// }
+
+// console.log(arrayFunction(myNewArray));
